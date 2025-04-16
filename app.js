e0a030b4cd56e411c3ffa413c5c6c434aec2a8d8
Sameed Hasan <sameedh41@gmail.com>
2025-04-16 11:04:06 +0500

    feat: default middlewares added

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware)
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
 app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 
   await connectToDatabase()
 });
