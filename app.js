654e29e0921adae8f34ce85bbe4974a5cd119c64
Sameed Hasan <sameedh41@gmail.com>
2025-04-16 10:46:12 +0500

    feat: mongo db integration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 app.use("/api/v1/auth", authRouter);
@@ -13,6 +14,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+
+  await connectToDatabase()
 });
